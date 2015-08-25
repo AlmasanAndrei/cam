70417ddcb5500cc2e328fb7755ead3a133e76999
almas <almasanandreii@gmail.com>
2015-08-25 21:34:43 +0300

    love ya

---
+++ app/src/main/java/com/example/kw/cam/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.kw.cam;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int Camera_request = 7;
+    private Upload upload; // Upload object containging image and meta data
+    private File chosenFile; //chosen file from intent
+      @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+public void buttonclicked(  View v){
+    Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, Camera_request)
+}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+            if(requestCode==Camera_request){
+                Bitmap cameraimg = (Bitmap) data.getExtras().get("data");
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    }
+    public void uploadImage() {
+    /*
+      Create the @Upload object
+     */
+        if (chosenFile == null) return;
+        createUpload(chosenFile);
+
+    /*
+      Start upload
+     */
+        new UploadService(this).Execute(upload, new UiCallback());
+    }
+
+    private void createUpload(File image) {
+        upload = new Upload();
+
+        upload.image = image;
+        upload.title = uploadTitle.getText().toString();
+        upload.description = uploadDesc.getText().toString();
+    }
+
+    private class UiCallback implements Callback<ImageResponse> {
+
+        @Override
+        public void success(ImageResponse imageResponse, Response response) {
+            clearInput();
+        }
+
+        @Override
+        public void failure(RetrofitError error) {
+            //Assume we have no connection, since error is null
+            if (error == null) {
+                Snackbar.make(findViewById(R.id.rootView), "No internet connection", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
+}//love ya
